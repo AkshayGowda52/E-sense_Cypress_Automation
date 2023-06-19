57be12eeaaa92861bced47e735e9471f5b3ac431
karthiktesyantra <karthik.k@testyantra.com>
2023-06-19 12:55:17 +0530

    19 manohara

--- cypress/e2e/LMS/Teacher_noticeboard.cy.js
+++ cypress/e2e/LMS/Teacher_noticeboard.cy.js
@@ -14,12 +14,31 @@ describe("Teacher Notice board Validation", function () {
 
     it('E2E_01 Verify that School Teacher sees "No Notice Found" text when notices were not published', function () {
         teacherDashboardPage.getNoticeboardSideBar().click({force:true})
-        cy.contains('No Notice Found').should('be.visible')
         teacherNoticeboardPage.getSearchTextfield().should('be.visible')
         teacherNoticeboardPage.getCalenderIcon().should('be.visible')
         teacherNoticeboardPage.getAcademicYearDropdown().should('be.visible')
         teacherNoticeboardPage.getSortByDropdown().should('be.visible')
-        
+
     })
 
+
+    it('E2E_02 Verify that School Teacher  can see the School Notice Board successfully', function () {
+        teacherDashboardPage.getNoticeboardSideBar().click({force:true})
+        teacherNoticeboardPage.getTeacherNoticesList().should('be.visible')
+        teacherNoticeboardPage.getNoticeTitle().should('be.visible')
+        teacherNoticeboardPage.getNoticeDate().should('be.visible')
+        teacherNoticeboardPage.getNoticeAttachment().should('be.visible').last().click()
+        teacherNoticeboardPage.getNoticeAttached().should('be.visible').click()
+        teacherNoticeboardPage.getNoticeAttachedPreviewCloseBtn().click()
+        cy.get('body').click()
+        teacherNoticeboardPage.getSearchTextfield().type('manohara')
+        teacherNoticeboardPage.getTeacherNoticesList().should('be.visible')
+        teacherNoticeboardPage.getSortByDropdown().should('be.visible')
+        teacherNoticeboardPage.getSortByDropdown().click({force:true})
+        teacherNoticeboardPage.getSortByDropdownGenaralOpt().click({timeout:4000})
+        teacherNoticeboardPage.getTeacherNoticesList().should('be.visible')
+        teacherNoticeboardPage.getSortByDropdown().should('be.visible').click()
+        teacherNoticeboardPage.getSortByDropdownAllOpt().click()
+        teacherNoticeboardPage.getTeacherNoticesList().should('be.visible')
+    })
 })
--- cypress/support/pageObjects/LMS/teacherNoticeboardPage.js
+++ cypress/support/pageObjects/LMS/teacherNoticeboardPage.js
@@ -13,7 +13,40 @@ class teacherNoticeboardPage{
     }
 
     getSortByDropdown(){
-        return cy.get('div[class="noticeSchoolSort"]')
+        return cy.get('.noticeSchoolSort > .MuiOutlinedInput-root > .MuiSelect-select')
+    }
+
+    getSortByDropdownGenaralOpt(){
+        return cy.get('li[data-value="GENERAL"]')
+    }
+
+    getSortByDropdownAllOpt(){
+        return cy.get('li[data-value="All"]')
+    }
+
+    getTeacherNoticesList(){
+        return cy.get('div[class="StudentSchool_schNotBordListSect__2vnwT MuiBox-root css-0"]')
+    }
+
+    getNoticeTitle(){
+        return cy.get('p.StudentSchool_schNotBordListTitle__2JyUo')
+    }
+
+    getNoticeDate(){
+        return cy.get('div.StudentSchool_schNotBordInfo__242JJ')
+    }
+
+    getNoticeAttachment(){
+        return cy.get('img[aria-label="View Attachment"]')
+    }
+
+    getNoticeAttached(){
+        return cy.get('div.attachmentListWrapperBlkText')
+    }
+
+    
+    getNoticeAttachedPreviewCloseBtn(){
+        return cy.get('svg[data-testid="CloseIcon"]')
     }
 
 }module.exports = new teacherNoticeboardPage();
